19c0fa7d52a38f41943191b52904b0598791e046
michelecolazzo01 <michele.colazzo@mail.polimi.it>
2019-05-06 17:35:26 +0200

    implemented client side connection test main

--- src/main/java/it/polimi/ingsw/client/controller/ClientPlayer.java
+++ src/main/java/it/polimi/ingsw/client/controller/ClientPlayer.java
@@ -7,7 +7,11 @@ import it.polimi.ingsw.server.model.Cell;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.SQLOutput;
 import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 /**
  * 
@@ -65,9 +69,21 @@ public class ClientPlayer extends UnicastRemoteObject implements ClientFunctiona
 	/**
 	 * 
 	 */
-	public static void main(){
+	public static void main(String[] args) {
+		String ip = args[0];
+		int port= Integer.parseInt(args[1]);
+		Scanner in= new Scanner(System.in);
+		System.out.println("Insert nickname: ");
+		String nickname= in.next();
+		System.out.println("Select connection type [0: RMI, 1: SOCKET]: ");
+		ConnectionType connectionType = (Integer.parseInt(in.next()) == 0) ? ConnectionType.SOCKET : ConnectionType.RMI;
 
+		try {
+			ClientPlayer clientPlayer = new ClientPlayer(nickname, connectionType, ip, port);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			exit(1);
+		}
 	}
-
 
 }
